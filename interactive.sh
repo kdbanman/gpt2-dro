bc89c40a2f8cb0c2159b6acb7c1803094d6ae6a4
Kirby Banman <kdbanman@ualberta.ca>
2023-06-08 19:05:46 -0700

    add slurm launcher WIPs

---
+++ interactive.sh
@@ -0,0 +1,1 @@
+salloc --time=1:0:0 --account=def-nidhih --nodes=1 --gpus-per-node=4 --exclusive
---
+++ slurm_launch.sh
@@ -0,0 +1,26 @@
+
+rsync -av tokenized-openwebtext $SLURM_TMPDIR/
+
+module load StdEnv/2020  gcc/9.3.0  cuda/11.4 python/3.10  arrow/11
+
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+
+pip install -U pip
+pip install --no-index -r requirements.txt
+
+
+# get interactive job on v100 node
+# generate accelerate config
+# run from test config with accelerate launche... (should be in launch script)
+# ensure runs to at least first eval step, huggingface push
+
+# write SGD config : cvar alpha 1.0
+
+accelerate launch gpt2-openwebtext-dro.py config-1.0.json $SLURM_TMPDIR
+
+# start run
+# pay attention to tqdm predicted finish time (on karma: 44hrs to complete 66% of an originally 47 hour estimate --> 67 hrs to finish --> multiply original estimate by 1.43)
+# so 1.43 it for accuracy, then 1.5 it for safety
+# ask slurm for it
+# dump log to slurm tmpdir?  don't worry about  model repo, those are low frequency writes.
